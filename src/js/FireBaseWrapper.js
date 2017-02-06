18a211620ed6f3b3e0eab63807522eb333974186
karan <karan.p.vyas.2012@gmail.com>
2017-02-06 19:43:38 +0530

    Made firebase wrapper

---
+++ src/js/FireBaseWrapper.js
@@ -0,0 +1,59 @@
+export default class FireBaseWrapper {
+
+  constructor(){
+    this.user = {
+    }
+  }
+
+  setup = () => {
+    let config = {
+      apiKey: "AIzaSyCxEq46JH7-12DxpeWcLquxeRwzgV4_r18",
+      authDomain: "journal-and-planner.firebaseapp.com",
+      databaseURL: "https://journal-and-planner.firebaseio.com",
+      storageBucket: "journal-and-planner.appspot.com",
+      messagingSenderId: "337037813028"
+    };
+
+    firebase.initializeApp(config);
+    this.database = firebase.database();
+
+    let that = this;
+    return new Promise(
+      function (resolve, reject){
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+            that.user = user;
+            resolve(user);
+          } else {
+            console.log('not signed in, signing in now');
+            let provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(provider).then(function(result) {
+              that.user = result.user;
+              resolve(result);
+            }).catch(function(error) {
+              console.log(error);
+              reject(error);
+            });
+          }
+        });
+      }
+    );
+  }
+
+  write = (path, data) => {
+    return this.database.ref(path).set(data);
+  }
+
+  //see how to return yo.
+  read = (path) => {
+    database.ref(path).once('value').then(
+      function(snapshot){
+        return snapshot.val();
+      }
+    ).catch(function(error){
+      console.log(error);
+      return error;
+    })
+  }
+
+}
--- src/js/entry.js
+++ src/js/entry.js
@@ -8,4 +8,18 @@ import GoalBox from './GoalBox'
 import Journal from './Journal'
 import DatePicker from './DatePicker'
 
-render(<Journal />, document.querySelector('#JournalAndPlannerApp'));
+import FireBaseWrapper from './FireBaseWrapper'
+let FBW = new FireBaseWrapper();
+
+FBW.setup().then(
+  function(user){
+    console.log(user);
+  }
+).catch(
+  function(error){
+    console.log('error while setting up firebase, speaking from entry.js #123\n'+error);
+  }
+);
+
+
+// render(<Journal />, document.querySelector('#JournalAndPlannerApp'));
