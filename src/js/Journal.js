b001d39eef5dc3a80c51f84bcfa0acff5953476d
karan <karan.p.vyas.2012@gmail.com>
2017-02-06 14:26:55 +0530

    added journaling editor

--- src/js/Journal.js
+++ src/js/Journal.js
@@ -1,3 +1,5 @@
+//why even create a component for this?
+
 import React from 'react'
 
 export default class Journal extends React.Component {
@@ -5,6 +7,32 @@ export default class Journal extends React.Component {
   constructor(props){
     super(props);
 
+    this.state = {
+      journalHTML : ''
+    }
+  }
+
+  componentDidMount() {
+
+    //TODO any better way for this?
+    let editor = document.createElement('div');
+    editor.id = "journalEditor";
+    document.querySelector('body').appendChild(editor);
+
+    var quill = new Quill('#journalEditor', {
+      theme: 'snow'
+    });
+
+    quill.on('text-change', function(a,b,c){
+      this.setState({journalHTML: quill.container.firstChild.innerHTML});
+    }.bind(this));
+  }
+
+  render(){
+    return (
+      <div>
+      </div>
+    )
   }
 
 }
--- src/js/entry.js
+++ src/js/entry.js
@@ -5,5 +5,6 @@ import React from 'react'
 import ContentEditable from './ContentEditable'
 import {render} from 'react-dom'
 import GoalBox from './GoalBox'
+import Journal from './Journal'
 
 render(<Journal />, document.querySelector('#JournalAndPlannerApp'));
