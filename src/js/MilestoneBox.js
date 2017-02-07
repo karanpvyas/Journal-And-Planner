d664a099f8d74f153c0dc5aee74a3ca73d5fbc11
karan <karan.p.vyas.2012@gmail.com>
2017-02-07 14:40:53 +0530

    added milestone box and firebase into it

--- src/js/MilestoneBox.js
+++ src/js/MilestoneBox.js
@@ -7,23 +7,57 @@ export default class MilestoneBox extends React.Component {
   constructor(props){
     super(props)
 
-    this.state = {
-      milestones: [
-        {
-          milestoneText: '',
-          milestoneDate: ''
+    this.props.FBW.read('milestones/').then(
+      (data) => {
+        console.log(data);
+        if(data == null){
+          this.setState({});
+        }else{
+          this.setState({milestones: JSON.parse(data)})
         }
-      ]
+      }
+    )
+
+    this.state= {
+      milestones: []
     }
+
+    //this is the signature
+    // this.state = {
+    //   milestones: [
+    //     {
+    //       milestoneText: '',
+    //       milestoneDate: ''
+    //     }
+    //   ]
+    // }
   }
 
   componentDidMount(){
   }
 
+  _setState = (milestones) => {
+    this.props.FBW.write('milestones/', JSON.stringify(milestones)).then(() => {
+      this.setState({milestones: milestones});
+    })
+  }
+
+  addMilestone = () => {
+    let milestoneTextObj = document.querySelector('#newMilestoneTextInput');
+    let milestoneText = milestoneTextObj.value;
+    let milestoneDate = this.refs.DatePickerReact.state.date._d.toLocaleString().split(',')[0];
+    let milestones = this.state.milestones;
+    milestones.push({milestoneText, milestoneDate})
+    milestones.sort((a ,b) => {
+      return a.milestoneDate.split('/').reverse().join() < b.milestoneDate.split('/').reverse().join()
+    }) // so that milestones appear chronologically.
+    this._setState(milestones)
+  }
+
   render(){
     let allMilestones = this.state.milestones.map((milestone) => {
       return(
-        <div>
+        <div key={milestone.milestoneDate+Math.random()}>
         <span>
         {milestone.milestoneText}
         </span>
@@ -35,9 +69,11 @@ export default class MilestoneBox extends React.Component {
     })
     return(
       <div>
+      <DatePickerReact ref="DatePickerReact" />
       <input id = "newMilestoneTextInput" type="text" />
       <br />
       <button onClick={this.addMilestone}>add</button>
+      {allMilestones}
       </div>
     )
   }
